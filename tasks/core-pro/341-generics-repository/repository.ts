33eb30395d1fbd887b1aabf949873d4d3ab090d1
gkurek <grzegorz.tomasz.kurek@gmail.com>
2024-12-12 20:46:22 +0100

    add 341

--- tasks/core-pro/341-generics-repository/repository.ts
+++ tasks/core-pro/341-generics-repository/repository.ts
@@ -1,5 +1,46 @@
 import { DataAccess } from './DataAccess.ts';
 
+type TableName = 'users' | 'products';
+
+export class Repository<T extends User | Product> {
+  private dataAccess: DataAccess;
+  private tableName: TableName;
+
+  constructor(dataAccess: DataAccess, tableName: TableName) {
+    this.dataAccess = dataAccess;
+    this.tableName = tableName;
+  }
+
+  async getById(id: number) {
+    const query = `SELECT * FROM ${this.tableName} WHERE id = $1`;
+    const values = [id];
+    const res = await this.dataAccess.query<T>(query, values);
+    return res.rows[0];
+  }
+
+  async getAll() {
+    const query = `SELECT * FROM ${this.tableName}`;
+    const res = await this.dataAccess.query<T>(query);
+    return res.rows;
+  }
+
+  async insert(item: Omit<T, 'id'>): Promise<T> {
+    if (this.tableName === 'users') {
+      const { name, email } = item as Omit<User, 'id'>;
+      const query = 'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *';
+      const values = [name, email];
+      const res = await this.dataAccess.query<T>(query, values);
+      return res.rows[0];
+    } else {
+      const { name, price } = item as Omit<Product, 'id'>;
+      const query = 'INSERT INTO products (name, price) VALUES ($1, $2) RETURNING *';
+      const values = [name, price];
+      const res = await this.dataAccess.query<T>(query, values);
+      return res.rows[0];
+    }
+  }
+}
+
 export interface User {
   id: number;
   name: string;
@@ -69,5 +110,3 @@ export class ProductRepository {
     return res.rows[0];
   }
 }
-
-export class Repository<T extends { id?: number }> {}
